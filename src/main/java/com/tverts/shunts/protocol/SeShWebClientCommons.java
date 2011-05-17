d9e60e3b1cf13c940fbddda965d20b5277f42c23
abaukin <abaukin@3e691eb0-fc83-11df-9a5c-5135339fc320>
2011-05-17 14:47:41 +0000

    reinit-02$stable: migrated to Apache HTTP Components Client 4.4.1, Hibernate 3.6.4.Final.

--- src/main/java/com/tverts/shunts/protocol/SeShWebClientCommons.java
+++ src/main/java/com/tverts/shunts/protocol/SeShWebClientCommons.java
@@ -14,25 +14,31 @@ import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
 
 /* Apache HTTP Component (client) */
 
-import com.tverts.shunts.protocol.SeShProtocolBase.SeShSystemFailure;
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.RequestEntity;
-import org.apache.commons.httpclient.params.HttpClientParams;
-import static org.apache.commons.httpclient.params.HttpClientParams.*;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpMessage;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+
+import static org.apache.http.params.CoreConnectionPNames.*;
+import static org.apache.http.params.CoreProtocolPNames.*;
 
 /* com.tverts: shunts protocol */
 
 import com.tverts.shunts.protocol.SeShProtocolBase.SeShConnectionFailed;
 import com.tverts.shunts.protocol.SeShProtocolBase.SeShServletFailure;
+import com.tverts.shunts.protocol.SeShProtocolBase.SeShSystemFailure;
 
 /* com.tverts: support streams */
 
 import com.tverts.support.streams.Base64Decoder;
 import com.tverts.support.streams.Base64Encoder;
+
 
 /**
  * Implements HTTP conversation with this server.
@@ -47,40 +53,39 @@ import com.tverts.support.streams.Base64Encoder;
 public class   SeShWebClientCommons
        extends SeShWebClientBase
 {
-	/* protected: connection issues) */
+	/* protected: connection issues */
 
-	protected SeShResponse  sendRequest (
-	                    String      port,
-	                    SeShRequest request
-	                  )
+	protected SeShResponse  sendRequest(String port, SeShRequest request)
 	  throws SeShProtocolError, InterruptedException
 	{
 		if(port    == null) throw new IllegalArgumentException();
 		if(request == null) throw new IllegalArgumentException();
 
 		//0: set request URL
-		String     url  = createURL(port);
+		String   url  = createURL(port);
 		if(url == null) throw new IllegalStateException();
 
 		//1: create POST request
-		PostMethod post = new PostMethod(url);
-		setRequestHeaders(post);
+		HttpPost req = new HttpPost(url);
+		setRequestHeaders(req);
 
 		try
 		{
-			post.setRequestEntity(createRequestBody(request));
+			req.setEntity(createRequestBody(request));
 		}
 		catch(Exception e)
 		{
 			throw new SeShProtocolError(
-			  "error occured when serializing Self Shunt Request " +
+			  "Error occured during teh encoding Self Shunt Request " +
 			  "into the body of the POST HTTP request!", e);
 		}
 
 		//2: send the HTTP request
+		HttpResponse res;
+
 		try
 		{
-			getClient().executeMethod(post);
+			res = getClient().execute(req);
 		}
 		catch(java.net.ConnectException e)
 		{
@@ -89,21 +94,21 @@ public class   SeShWebClientCommons
 		catch(Exception e)
 		{
 			throw new SeShServletFailure(String.format(
-			  "error occured when send POST HTTP request to the" +
+			  "Error occured when send POST HTTP request to the" +
 			  "server by the URL: '%s'!", url), e);
 		}
 
 		if(breaked) throw new InterruptedException();
 
 		//3: handle the response status
-		handleResponseStatus(post);
+		handleResponseStatus(res);
 
-		SeShResponse res;
+		SeShResponse result;
 
 		//4: decode shunt response
 		try
 		{
-			res = readResponse(post);
+			result = readResponse(res);
 		}
 		catch(SeShProtocolError e)
 		{
@@ -112,22 +117,20 @@ public class   SeShWebClientCommons
 		catch(Exception e)
 		{
 			throw new SeShProtocolError(
-			  "error occured when deserializing Self Shunt " +
+			  "Error occured during the reading Self Shunt " +
 			  "Response instance from the HTTP stream!", e);
 		}
 
 		//5: {the response contains system error} throw it
-		if(res.getSystemError() != null)
-			throw new SeShSystemFailure(res);
+		if(result.getSystemError() != null)
+			throw new SeShSystemFailure(result);
 
-		return res;
+		return result;
 	}
 
-	/* protected: request coding and decoding  */
+	/* protected: request and response processing */
 
-	protected RequestEntity createRequestBody (
-	                          SeShRequest request
-	                        )
+	protected HttpEntity    createRequestBody(SeShRequest request)
 	  throws Exception
 	{
 		//create encoding streams
@@ -139,77 +142,129 @@ public class   SeShWebClientCommons
 		oos.writeObject(request);
 		oos.close();
 
-		//create the POST body entry
-		return new ByteArrayRequestEntity(
-		  bos.toByteArray(), "application/octet-stream");
+		//~: create the entity
+		ByteArrayEntity       res =
+		  new ByteArrayEntity(bos.toByteArray());
+
+		res.setContentType("application/octet-stream");
+		return res;
 	}
 
-	protected void          setRequestHeaders(PostMethod post)
+	protected void          setRequestHeaders(HttpMessage msg)
 	{
 		//Content-Type
-		post.addRequestHeader(
+		msg.addHeader(
 		  "Content-Type",
 		  "application/octet-stream"
 		);
 
 		//Content-Transfer-Encoding
-		post.addRequestHeader(
+		msg.addHeader(
 		  "Content-Transfer-Encoding",
 		  "base64"
 		);
 	}
 
-	protected SeShResponse  readResponse(HttpMethod req)
+	protected SeShResponse  readResponse(HttpResponse res)
 	  throws Exception
 	{
-		String            ctp = getContentType(req);
+		//~: check Content-Type header
+		checkResponseContentType(res);
+
+		//~: check Content-Transfer-Encoding header
+		checkResponseEncoding(res);
+
+		//~: open the content stream
+		InputStream       ins = openContentStream(res);
+		ObjectInputStream ois;
+		Exception         err = null;
+		Object            obj = null;
+
+		//~read the response object
+		try
+		{
+			ois = new ObjectInputStream(ins);
+			obj = ois.readObject();
+			ins = ois; //<-- prepare for close
+		}
+		catch(Exception e)
+		{
+			err = e;
+		}
+		finally
+		{
+			try
+			{
+				ins.close();
+			}
+			catch(Exception e)
+			{
+				if(err == null) err = e;
+			}
+		}
+
+		//?: {has stream error}
+		if(err != null) throw new SeShServletFailure(
+		  "Error occured while reading SeShResponse from the stream!", err);
+
+		//?: {the result object is undefined}
+		if(obj == null) throw new SeShServletFailure(
+		  "SeShResponse is not defined!");
+
+		//?: {wrong object type}
+		if(!(obj instanceof SeShResponse)) throw new SeShServletFailure(
+		  "Self Shunt HTTP response instance has wrong Java class type!");
+
+		return (SeShResponse)obj;
+	}
+
+	protected InputStream   openContentStream(HttpResponse res)
+	  throws Exception
+	{
+		InputStream ins;
+
+		//~: open the response' entity' content
+		try
+		{
+			ins = res.getEntity().getContent();
+			if(ins == null) throw new NullPointerException();
+		}
+		catch(Exception e)
+		{
+			throw new SeShServletFailure(
+			  "Can't read the content data from the HTTP response!", e);
+		}
+
+		//?: {has Base64 transfer encoding} wrap with the decoder
+		if("base64".equals(getTransferEncoding(res)))
+			ins = new Base64Decoder(ins);
+
+		return ins;
+	}
+
+	protected void          checkResponseContentType(HttpResponse res)
+	  throws Exception
+	{
+		String ct = getContentType(res);
 
 		//?: {content type is not defined}
-		if(ctp == null) throw new SeShServletFailure(
+		if(ct == null) throw new SeShServletFailure(
 		  "Self Shunt HTTP response has no Content-Type provided!");
 
 		//?: {wrong content type}
-		if(!"application/octet-stream".equals(ctp))
-			throw new SeShServletFailure(
-			  "Self Shunt HTTP response has the Content-Type with " +
-			  "not supported value!");
-
-		InputStream       ins = req.getResponseBodyAsStream();
-		String            ten = getTransferEncoding(req);
-
-		//?: {has Base64 encoding}
-		if("base64".equals(ten))
-			ins = new Base64Decoder(ins);
-		//?: {has unknown transfer encoding}
-		else if(ten != null) throw new SeShServletFailure(String.format(
-		  "Self Shunt HTTP response has the Content-Transfer-Encoding " +
-		  "header with unsupported value: '%s'", ten));
-
-		//read the response object
-		ObjectInputStream ois = new ObjectInputStream(ins);
-		Object            res = ois.readObject();
-
-		ois.close();
-
-		//?: {the result object is undefined}
-		if(res == null) throw new SeShServletFailure(
-		  "Self Shunt HTTP response is not defined!");
-
-		//?: {wrong object type}
-		if(!(res instanceof SeShResponse))
-			throw new SeShServletFailure(
-			  "Self Shunt HTTP response instance has wrong type!");
-
-		return (SeShResponse)res;
+		if(!"application/octet-stream".equals(ct)) throw new SeShServletFailure(
+		  "Self Shunt HTTP response has the Content-Type with " +
+		  "not supported value!");
 	}
 
 	/**
 	 * Finds the 'Content-Type' from the request given.
 	 * The result is turned to lower case.
 	 */
-	protected String        getContentType(HttpMethod req)
+	protected String        getContentType(HttpMessage msg)
 	{
-		Header hdr = req.getResponseHeader("Content-Type");
+		Header hdr = msg.getFirstHeader(
+		  "Content-Type");
 		if(hdr == null) return null;
 
 		String val = hdr.getValue();
@@ -219,15 +274,30 @@ public class   SeShWebClientCommons
 		return (ich != -1)?(val.substring(0, ich)):(val);
 	}
 
+	protected void          checkResponseEncoding(HttpResponse res)
+	  throws Exception
+	{
+		String te = getTransferEncoding(res);
+
+		//?: {has Base64 encoding}
+		if("base64".equals(te))
+			return;
+
+		//?: {has unknown transfer encoding}
+		if(te != null) throw new SeShServletFailure(String.format(
+		  "Self Shunt HTTP response has the Content-Transfer-Encoding " +
+		  "header with unsupported value: '%s'", te));
+	}
+
 	/**
 	 * Finds the 'Content-Transfer-Encoding'
 	 * from the request given.
 	 *
 	 * The result is turned to lower case.
 	 */
-	protected String        getTransferEncoding(HttpMethod req)
+	protected String        getTransferEncoding(HttpMessage msg)
 	{
-		Header hdr = req.getResponseHeader(
+		Header hdr = msg.getFirstHeader(
 		  "Content-Transfer-Encoding");
 		if(hdr == null) return null;
 
@@ -235,44 +305,45 @@ public class   SeShWebClientCommons
 		return (val == null)?(null):(val.toLowerCase());
 	}
 
-	protected void          handleResponseStatus(HttpMethod req)
+	protected void          handleResponseStatus(HttpResponse res)
 	  throws SeShProtocolError
 	{
 		//?: {got good status}
-		if(req.getStatusCode() == SC_OK)
+		if(res.getStatusLine().getStatusCode() == SC_OK)
 			return;
 
 		//?: {service is temporary unavailable}
-		if(req.getStatusCode() == SC_SERVICE_UNAVAILABLE)
+		if(res.getStatusLine().getStatusCode() == SC_SERVICE_UNAVAILABLE)
 			throw new SeShConnectionFailed();
 
 		//!: raise servlet failure
 		throw new SeShServletFailure(String.format(
 		  "Self Shunt response has bad HTTP status code %d, status text: \n%s",
-		  req.getStatusCode(), req.getStatusText()));
 
+		  res.getStatusLine().getStatusCode(),
+		  res.getStatusLine().getReasonPhrase()));
 	}
 
 	/* protected: HttpClient creation */
 
-	protected HttpClient       getClient()
+	protected HttpClient    getClient()
 	{
 		return (client != null)?(client)
 		  :(client = createClient());
 	}
 
-	protected HttpClient       createClient()
+	protected HttpClient    createClient()
 	{
-		return new HttpClient(buildClientParams());
+		return new DefaultHttpClient(buildClientParams());
 	}
 
-	protected HttpClientParams buildClientParams()
+	protected HttpParams    buildClientParams()
 	{
-		HttpClientParams p = new HttpClientParams();
+		HttpParams p = new BasicHttpParams();
 
-		//URI encoding
+		//headers encoding
 		p.setParameter(
-		  HTTP_URI_CHARSET,           "UTF-8");
+		  HTTP_ELEMENT_CHARSET,       "UTF-8");
 
 		//content encoding
 		p.setParameter(
@@ -283,18 +354,8 @@ public class   SeShWebClientCommons
 		  SO_TIMEOUT,                 getSoTimeout());
 
 		//connection timeout
-		p.setLongParameter(
-		  CONNECTION_MANAGER_TIMEOUT, getConnTimeout());
-
-		//redirects
-		p.setBooleanParameter(
-		  ALLOW_CIRCULAR_REDIRECTS,   false);
-
-		p.setBooleanParameter(
-		  REJECT_RELATIVE_REDIRECT,   false);
-
 		p.setIntParameter(
-		  MAX_REDIRECTS,              2);
+		  CONNECTION_TIMEOUT,         getConnTimeout());
 
 		return p;
 	}
