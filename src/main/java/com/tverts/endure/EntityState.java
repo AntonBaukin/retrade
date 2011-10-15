149614c2d3df3c7f68e2609a2aea442ed22875a1
abaukin <abaukin@3e691eb0-fc83-11df-9a5c-5135339fc320>
2011-10-15 07:39:35 +0000

    ui-04$alpha: added document states.

---
+++ src/main/java/com/tverts/endure/EntityState.java
@@ -0,0 +1,13 @@
+package com.tverts.endure;
+
+/**
+ * Interface of a state a stateful entity has.
+ *
+ * @author anton.baukin@gmail.com
+ */
+public interface EntityState
+{
+	/* public: EntityState interface */
+
+	public UnityType getStateType();
+}
---
+++ src/main/java/com/tverts/endure/StatefulEntity.java
@@ -0,0 +1,13 @@
+package com.tverts.endure;
+
+/**
+ * Defines that entity has a state.
+ *
+ * @author anton.baukin@gmail.com
+ */
+public interface StatefulEntity
+{
+	/* public: StatefulEntity interface */
+
+	public EntityState getEntityState();
+}
