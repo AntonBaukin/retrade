db8740c2e28b3bbcc082007e42cf15fff1bfb332
abaukin <abaukin@3e691eb0-fc83-11df-9a5c-5135339fc320>
2013-01-11 14:04:08 +0000

    reinit-12$alpha: reimplemented Hibery integration as Hibernate Integrator.

---
+++ src/main/java/com/tverts/hibery/system/HiberSystemIntegration.java
@@ -0,0 +1,51 @@
+package com.tverts.hibery.system;
+
+/* Hibernate Persistence Layer */
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.integrator.spi.Integrator;
+import org.hibernate.metamodel.source.MetadataImplementor;
+import org.hibernate.service.spi.SessionFactoryServiceRegistry;
+
+
+/**
+ * Integrates Hibernate System with the application.
+ *
+ * @author anton.baukin@gmail.com
+ */
+public class HiberSystemIntegration implements Integrator
+{
+	public void integrate(
+	    Configuration cfg, SessionFactoryImplementor sf,
+	    SessionFactoryServiceRegistry sr)
+	{
+		//~: save the configuration
+		HiberSystem.getInstance().setConfiguration(cfg);
+
+		//~: register the event listeners
+		registerEventListeners(
+			sr.getService(EventListenerRegistry.class)
+		);
+	}
+
+	public void integrate(
+	   MetadataImplementor metadata,
+	   SessionFactoryImplementor sf,
+	   SessionFactoryServiceRegistry sr)
+	{}
+
+	public void disintegrate(
+	  SessionFactoryImplementor sf,
+	  SessionFactoryServiceRegistry sr)
+	{}
+
+
+	/* protected: integration */
+
+	protected void registerEventListeners(EventListenerRegistry er)
+	{
+		//TODO add SetTxHiberyListener
+	}
+}
---
+++ src/main/java/com/tverts/hibery/system/SetTxHiberyListener.java
@@ -0,0 +1,30 @@
+package com.tverts.hibery.system;
+
+/* Hibernate Persistence Layer */
+
+import org.hibernate.event.spi.SaveOrUpdateEvent;
+import org.hibernate.event.spi.SaveOrUpdateEventListener;
+
+/* com.tverts: system (tx) */
+
+import com.tverts.system.tx.Tx;
+
+
+/**
+ * Listener that installs Transaction number
+ * of current {@link Tx} into the saved or
+ * updated instances.
+ *
+ *
+ * @author anton.baukin@gmail.com
+ */
+public class      SetTxHiberyListener
+       implements SaveOrUpdateEventListener
+{
+	/* public: SaveOrUpdateEventListener interface */
+
+	public void onSaveOrUpdate(SaveOrUpdateEvent event)
+	{
+
+	}
+}
