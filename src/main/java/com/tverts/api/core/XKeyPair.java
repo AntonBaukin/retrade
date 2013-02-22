992288fe3f2e90f73926250d2b2c1522aeb4204e
abaukin <abaukin@3e691eb0-fc83-11df-9a5c-5135339fc320>
2013-02-22 06:52:25 +0000

    i1c-00$alpha: extracted into BeanInfo classes metadata. Introduced support for foreign keys comparing.

---
+++ src/main/java/com/tverts/api/core/XKeyPair.java
@@ -0,0 +1,37 @@
+package com.tverts.api.core;
+
+/* standard Java classes */
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+/**
+ * When integrating, a foreign key to an entity
+ * has the value (Long) from ReTrade system,
+ * and it is not known to an external system.
+ *
+ * External system do refer the entity by
+ * it's own key, the x-keys pair.
+ *
+ * Integrating application links the pair.
+ *
+ * Mark with it a get-method of the Retrade Long
+ * primary key. The x-keys pair property of then
+ * must start with 'X'. For example, getMeasure()
+ * and getXMeasure().
+ *
+ * The x-key property must be string.
+ */
+@Target(METHOD) @Retention(RUNTIME)
+public @interface XKeyPair
+{
+	/**
+	 * Defines API type of the entity referred.
+	 * Used by integrating applications to
+	 * links the x-keys pair.
+	 */
+	Class type();
+}
