c8802b8e0a0b753585c66d6e478bf3d534492bdb
abaukin <abaukin@3e691eb0-fc83-11df-9a5c-5135339fc320>
2014-07-09 13:03:07 +0000

    bakery-06 [alpha]: moved API Firm from ReTrade module to Root.

---
+++ src/main/java/com/tverts/api/clients/Firm.java
@@ -0,0 +1,87 @@
+package com.tverts.api.clients;
+
+/* Java API for XML Binding */
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+/* com.tverts: api */
+
+import com.tverts.api.core.CatItem;
+
+
+/**
+ * A Contractor is being a Firm.
+ */
+@XmlType(name = "firm", propOrder = {
+  "fullName", "taxNumber", "taxCode",
+  "addressString", "phonesString"
+})
+public class Firm extends CatItem
+{
+	@XmlElement(name = "full-name")
+	public String getFullName()
+	{
+		return fullName;
+	}
+
+	private String fullName;
+
+	public void setFullName(String fullName)
+	{
+		this.fullName = fullName;
+	}
+
+	@XmlElement(name = "tax-number")
+	public String getTaxNumber()
+	{
+		return taxNumber;
+	}
+
+	public void setTaxNumber(String taxNumber)
+	{
+		this.taxNumber = taxNumber;
+	}
+
+	@XmlElement(name = "tax-code")
+	public String getTaxCode()
+	{
+		return taxCode;
+	}
+
+	public void setTaxCode(String taxCode)
+	{
+		this.taxCode = taxCode;
+	}
+
+	@XmlElement(name = "address-string")
+	public String getAddressString()
+	{
+		return addressString;
+	}
+
+	public void setAddressString(String addressString)
+	{
+		this.addressString = addressString;
+	}
+
+	@XmlElement(name = "phones-string")
+	public String getPhonesString()
+	{
+		return phonesString;
+	}
+
+	public void setPhonesString(String phonesString)
+	{
+		this.phonesString = phonesString;
+	}
+
+
+	/* attributes */
+
+
+	private String taxNumber;
+	private String taxCode;
+	private String addressString;
+	private String phonesString;
+}
