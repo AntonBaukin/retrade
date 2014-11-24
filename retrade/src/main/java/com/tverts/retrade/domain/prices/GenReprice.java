ad343ba38ba964fd4e59d810ef67a530b614c262
abaukin <abaukin@3e691eb0-fc83-11df-9a5c-5135339fc320>
2014-11-24 18:22:07 +0000

    domain-43 [alpha]: generating Price Change Documents with Price List' positions add and remove requests issued.

--- retrade/src/main/java/com/tverts/retrade/domain/prices/GenReprice.java
+++ retrade/src/main/java/com/tverts/retrade/domain/prices/GenReprice.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 /* com.tverts: spring */
@@ -90,7 +91,7 @@ public class      GenReprice
 
 	/* Generate Price Change Document */
 
-	@Param
+	@Param(descr = "The minimum number of the goods of the price list to change")
 	public Integer getMinGoods()
 	{
 		return minGoods;
@@ -104,7 +105,7 @@ public class      GenReprice
 		this.minGoods = n;
 	}
 
-	@Param
+	@Param(descr = "The maximum number of the goods of the price list to change")
 	public Integer getMaxGoods()
 	{
 		return maxGoods;
@@ -118,7 +119,8 @@ public class      GenReprice
 		this.maxGoods = n;
 	}
 
-	@Param
+	@Param(descr = "Integer (0; 100) of the good price delta " +
+	  "related to the main price list")
 	public int getCostsDelta()
 	{
 		return costsDelta;
@@ -130,6 +132,21 @@ public class      GenReprice
 	{
 		EX.assertx((n > 0) & (n < 100));
 		this.costsDelta = n;
+	}
+
+	@Param(descr = "Integer [0; 100] of the persent of the goods not in " +
+	  "the main price list to just update, not delete or add")
+	public int getUpdatePercent()
+	{
+		return updatePercent;
+	}
+
+	private int updatePercent = 80;
+
+	public void setUpdatePercent(int n)
+	{
+		EX.assertx((n >= 0) & (n <= 100));
+		this.updatePercent = n;
 	}
 
 
@@ -173,8 +190,6 @@ public class      GenReprice
 
 	protected void genPriceChanges(GenCtx ctx, RepriceDoc rd)
 	{
-		GetPrices get = bean(GetPrices.class);
-
 		EX.assertx(rd.getChanges().isEmpty());
 		for(GoodPrice gp : genSelectGoods(ctx, rd))
 		{
@@ -188,55 +203,126 @@ public class      GenReprice
 			//=: good unit
 			pc.setGoodUnit(gp.getGoodUnit());
 
-			//~: variate the cost value
-			int        d = 100 - costsDelta + ctx.gen().nextInt(2*costsDelta + 1);
-			BigDecimal c = gp.getPrice().multiply(new BigDecimal(d)).scaleByPowerOfTen(-2);
+			//?: {the good is not removed}
+			if(gp.getPrice() != null)
+			{
 
-			//?: {scale the cost to .XY}
-			if(c.scale() != 2)
-				c = c.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+				//~: variate the cost value
+				int d = 100 - costsDelta + ctx.gen().nextInt(2*costsDelta + 1);
+				BigDecimal c = gp.getPrice().multiply(new BigDecimal(d)).scaleByPowerOfTen(-2);
 
-			//=: the new price
-			pc.setPriceNew(c);
+				//?: {scale the cost to .XY}
+				if(c.scale() != 2)
+					c = c.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+				//=: the new price
+				pc.setPriceNew(c);
+			}
 		}
 	}
 
 	protected List<GoodPrice> genSelectGoods(GenCtx ctx, RepriceDoc rd)
 	{
-		//HINT: main price list always contains the prices of all the
-		// goods to sell, and no good position may be removed or added!
+		//HINT: main price list always contains the prices of all the goods
+		//  to sell, and no good position may be removed or added there!
 
 		//~: the target price list
 		PriceListEntity pl = EX.assertn(rd.getPriceList());
 
-//		//~: the main price list
-//		PriceListEntity mn = EX.assertn(
-//		  ctx.get(PriceListEntity.class),
-//		  "No Main Price List is found (generated)!"
-//		);
-//
-//		//~: all the goods from the main price list
-//		List<GoodPrice> ps = bean(GetPrices.class).getPriceListPrices(mn);
-//		EX.asserte(ps, "Main Price List has no prices generated!");
+		//~: the main price list
+		PriceListEntity mn = EX.assertn(
+		  ctx.get(PriceListEntity.class),
+		  "No Main Price List is found (generated)!"
+		);
 
-		//TODO: implement add and remove operations of Price List price changes
-
-		List<GoodPrice> ps = bean(GetPrices.class).getPriceListPrices(pl);
+		//~: all the goods from the main price list
+		List<GoodPrice> ps = bean(GetPrices.class).getPriceListPrices(mn);
+		EX.asserte(ps, "Main Price List has no prices generated!");
 
 		//~: shuffle them
 		Collections.shuffle(ps, ctx.gen());
 
-		//~: generate the number of goods
-		Integer min = this.getMinGoods();
-		if(min == null) min = 1;
-		Integer max = this.getMaxGoods();
-		if(max == null) max = ps.size();
-		if(max < min) max = min;
-		if(max > ps.size()) max = ps.size();
+		//~: the number of goods to process
+		int num;
+		{
+			Integer min = this.getMinGoods();
+			if(min == null) min = 1;
+			Integer max = this.getMaxGoods();
+			if(max == null) max = ps.size();
+			if(max < min) max = min;
+			if(max > ps.size()) max = ps.size();
 
-		//~: the sub-list of the prices
-		int num = min + ctx.gen().nextInt(max - min + 1);
-		ps = ps.subList(0, num);
+			num = min + ctx.gen().nextInt(max - min + 1);
+		}
+
+		//?: {this is the main price list} variate the prices
+		if(mn.equals(pl))
+			//~: just a sub-list of the prices
+			ps = ps.subList(0, num);
+		//~: update some of the existing prices, add or remove
+		else
+		{
+			//~: the resulting list
+			List<GoodPrice> xr = new ArrayList<>(num);
+
+			//~: map all the goods with the prices
+			HashMap<Long, GoodPrice> pm = new HashMap<>(ps.size());
+			for(GoodPrice p : ps)
+				pm.put(p.getGoodUnit().getPrimaryKey(), p);
+
+			//~: the current prices of the list
+			List<GoodPrice> cr = bean(GetPrices.class).getPriceListPrices(pl);
+			Collections.shuffle(ps, ctx.gen()); //<-- shuffle them
+
+			//~: exclude them from add candidates
+			for(GoodPrice p : cr)
+				pm.remove(p.getGoodUnit().getPrimaryKey());
+
+			//~: first, take the goods to just update
+			int up = updatePercent * Math.min(cr.size(), num)  / 100;
+			if(up != 0) xr.addAll(cr.subList(0, up));
+
+			//~: then, process add-remove (what is possible)
+			for(int i = xr.size();(i < num);i++)
+			{
+				//?: {add is possible}
+				boolean ap = !pm.isEmpty();
+
+				//?: {remove is possible}
+				boolean rp = (up < cr.size());
+
+				//?: {both are not possible}
+				EX.assertx(ap | rp, "Both add and remove operatins are not possible ",
+				  "for Price List [", pl.getPrimaryKey(), "] with code [", pl.getCode(), "]!");
+
+				//?: {to add (false) or to remove (true)}
+				boolean ar = (ap & rp)?(ctx.gen().nextBoolean()):(!ap);
+
+				//?: {remove}
+				if(ar)
+				{
+					//~: take the item existing in the price list
+					GoodPrice p = cr.get(up++);
+					GoodPrice x = new GoodPrice();
+
+					x.setGoodUnit(p.getGoodUnit());
+					x.setPrice(null); //<-- mark to remove it
+
+					xr.add(x);
+				}
+				else
+				{
+					//~: take the item not existing in the list
+					GoodPrice p = pm.values().iterator().next();
+					pm.remove(p.getGoodUnit().getPrimaryKey());
+
+					xr.add(p);
+				}
+			}
+
+			//~: the resulting list
+			ps = xr;
+		}
 
 		//~: the result prices
 		List<GoodPrice> res = new ArrayList<>(ps.size());
@@ -244,7 +330,7 @@ public class      GenReprice
 		{
 			GoodPrice x; res.add(x = new GoodPrice());
 			x.setGoodUnit(gp.getGoodUnit());
-			x.setPrice(EX.assertn(gp.getPrice()));
+			x.setPrice(gp.getPrice()); //<-- price is null when remove
 		}
 
 		return res;
