60026faaca7fdc67f0f6e8efe8da00ad03c652e2
abaukin <abaukin@3e691eb0-fc83-11df-9a5c-5135339fc320>
2015-03-04 07:00:36 +0000

    system-11 [stable]: applied RSync explode for Auth module. Using the same Data Source for Auth as for ReTrade.

---
+++ explode-rsync.bat
@@ -0,0 +1,2 @@
+..\dev\rsync\sshpass.exe -p "%1" ..\dev\rsync\rsync.exe -rtz "--chown=omicron:www" "--chmod=g=rwx" -e "..\dev\rsync\ssh.exe -oStrictHostKeyChecking=no" "target/web/" "omicron@%2:/home/tomee/webapps/auth/"
+@exit
