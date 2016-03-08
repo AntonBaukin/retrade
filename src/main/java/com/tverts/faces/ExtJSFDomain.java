0d56fac48ce240a49254dbb55551ff8846c031e9
abaukin <abaukin@3e691eb0-fc83-11df-9a5c-5135339fc320>
2016-03-08 10:26:54 +0000

    ui-101 [alpha]: replaced ${rootView.extjsfDomain} with simple ${extDom}.

---
+++ src/main/java/com/tverts/faces/ExtJSFDomain.java
@@ -0,0 +1,31 @@
+package com.tverts.faces;
+
+/* Spring Framework */
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/* com.tverts: spring */
+
+import static com.tverts.spring.SpringPoint.bean;
+
+
+/**
+ * Class-variable. Shortcut for
+ * {@link RootView#getExtjsDomain()}.
+ *
+ * @author anton.baukin@gmail.com.
+ */
+@Component("extDom") @Scope("request")
+public class ExtJSFDomain
+{
+	/* Object */
+
+	public String toString()
+	{
+		return (domain != null)?(domain):
+		  (domain = bean(RootView.class).getExtjsDomain());
+	}
+
+	private String domain;
+}
